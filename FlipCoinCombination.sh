f325f8991534e1fbafdb3a49062087fd450d663c
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-07-08 16:46:12 +0530

    Per

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,11 +1,32 @@
 
 
+countH=0
+countT=0
+for((i=0;i<15;i++))
+do
+
 num=$((RANDOM%2))
 
 
 if [ $num -eq 0 ]
   then
      echo "HEAD WINS"
+           countH=$((countH+1))
 else
      echo "TAILS WINS"
+           countT=$((countT+1))
 fi
+done
+
+echo ""$countH
+echo ""$countT
+
+if [ $countH -gt $countT ]
+   then
+       percent=$((100*$countH/15))
+else
+       percent=$((100*$countT/15))
+fi
+
+echo "Percent is - " $percent"%"
+
